47649542930ed418ef8234faab73a937b703ea00
faukisatu <37434531+faukisatu@users.noreply.github.com>
2018-06-28 02:06:03 +0700

    Add files via upload

---
+++ fauki/connection.php
@@ -0,0 +1,7 @@
+<?php
+    $server=”localhost”;
+    $user=”root”;
+    $password=”root”;
+    mysql_connect(“localhost”,”root”,”root”) or die (mysql_error());
+    mysql_select_db(“jadwal_kuliah”) or die (mysql_error());
+?>
