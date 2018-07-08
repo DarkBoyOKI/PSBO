b961b30f8d6470834bf858671a8e59820649276b
DarkBoyOKI <okimonoki@rocketmail.com>
2018-07-08 11:29:13 +0700

    reorganize things

--- fauki/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $server=”localhost”;
-    $user=”root”;
-    $password=”root”;
-    mysql_connect(“localhost”,”root”,”root”) or die (mysql_error());
-    mysql_select_db(“jadwal_kuliah”) or die (mysql_error());
-?>
--- fauki/hapus_jadwal.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $connection = mysql_connect(“localhost”,”root”,”root”) or die (mysql_error());
-    mysql_select_db(“jadwal_kuliah”) or die (mysql_error());
-    $id_jadwal = $_GET[‘id_jadwal’];
-    mysql_query(“DELETE FROM `jadwal` WHERE `jadwal`.`id_jadwal` = ‘$id_jadwal'”) or die (mysql_error());
-    mysql_close($connection);
-?>
-<p>jadwal kuliah sudah dihapus silahkan lihat <a href=”tampil.php”>disini</a></p>
