7828195979346a5fd2af18d1991a30019777b21f
DarkBoyOKI <okimonoki@rocketmail.com>
2018-07-10 04:58:07 +0700

    repair some files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\facades\schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,9 +12,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+
     public function boot()
     {
-        //
         Schema::defaultStringLength(191);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -15,27 +15,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-//->middleware('auth');
+    // Authentication Routes...
+Route::get('admin/home', 'AdminController@index');
+    $this->get('admin', 'Admin\LoginController@showLoginForm')->name('admin.login');
+    $this->post('admin', 'Admin\LoginController@login');
+    $this->post('logout', 'Admin\LoginController@logout')->name('logout');
+
+    // Registration Routes...
+    //$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    //$this->post('register', 'Auth\RegisterController@register');
+
+    // Password Reset Routes...
+    $this->get('admin-password/reset', 'Admin\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+    $this->post('admin-password/email', 'Admin\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+    $this->get('admin-password/reset/{token}', 'Admin\ResetPasswordController@showResetForm')->name('admin.password.reset');
+    $this->post('admin-password/reset', 'Admin\ResetPasswordController@reset');
 
 
-Route::middleware(['auth'])->group(function () {
-
+Route::middleware(['auth'])->group(function (){
     Route::resource('jadwals', 'JadwalsController');
-
     Route::get('projects/create/{jadwal_id?}', 'ProjectsController@create');
     Route::post('/projects/adduser', 'ProjectsController@adduser')->name('projects.adduser');
     Route::resource('projects', 'ProjectsController');
-    
+        
     Route::resource('roles', 'RolesController');
     Route::resource('tasks', 'TasksController');
     Route::resource('users', 'UsersController');
     Route::resource('comments', 'CommentsController');
-
-    
 });
-
